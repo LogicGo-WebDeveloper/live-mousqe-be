c349e40f8e88bcce74cc110a0ac1a34e5d9b8785
@shreya.logicgo <shreya.logicgo@gmail.com>
2026-02-03 17:38:27 +0530

    upload files and preview in live domain

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoutes";
 import sectionRoutes from "./routes/sectionRoutes";
 import fileRoutes from "./routes/fileRoutes";
 import blogRoutes from "./routes/blogRoutes";
+import uploadRoutes from "./routes/uploadRoutes";
 import path from "path";
 
 dotenv.config();
@@ -25,7 +26,7 @@ app.use(
       ...(process.env.CORS_ORIGIN ? [process.env.CORS_ORIGIN] : []),
     ],
     credentials: true,
-  })
+  }),
 );
 
 // Log all the allowed CORS origins at startup
@@ -42,12 +43,14 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // also serve static uploads so you can view them
+app.use("/", express.static(path.join(__dirname, "../assets")));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.get("/api/health", (req, res) =>
   res.send("server is running on http://localhost:" + PORT)
 );
 
+app.use("/api/upload", uploadRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/sections", sectionRoutes);
 app.use("/api/files", fileRoutes);
